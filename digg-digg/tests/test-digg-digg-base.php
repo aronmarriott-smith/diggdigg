9cd14e27e47a5a1d732abf4283753cf38de80dad
vagrant <aron@atomace.com>
2016-11-30 22:52:58 +0000

    Added unit test

---
+++ digg-digg/tests/test-digg-digg-base.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Tests for our plugin.
+ *
+ * @package Digg_Digg_Plugin
+ */
+
+/**
+ * Digg_Digg_Base_Test
+ */
+class Digg_Digg_Base_Test extends Digg_Digg_Plugin_UnitTestCase {
+
+	/**
+	 * Holds an instance of our plugin class.
+	 *
+	 * @var DDBase $instance
+	 */
+	private $instance;
+
+	/**
+	* This method is called before every single test is run.
+	*/
+	public function setUp() {
+		parent::setUp();
+
+		$base_url = '<a href="://api.example.com/share">example.com</a>';
+		$this->instance = new DDBase( 'Example', 'example.com', 'api.example.com', $base_url );
+	}
+
+	/**
+	 * This method is called after every single test is run.
+	 */
+	public function tearDown() {
+		parent::tearDown();
+	}
+}
